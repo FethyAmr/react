ed8d93f8096293e4a4b3c344f7f17e3e8f3cec56
FethyAmr <74984977+FethyAmr@users.noreply.github.com>
2021-04-07 15:44:52 +0200

    02-Affichage

--- chapitre-01/elsy/src/App.js
+++ chapitre-01/elsy/src/App.js
@@ -12,7 +12,15 @@ const stepsMax = 50000;
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className="container-fluid">
+        <div className="row">
+         
+          <p>Heart {heartMin} </p>
+          <p>Temperature {tempMin} </p>
+          <p>Steps {stepsMin} </p>
+
+        </div>
+
       </div>
     );
   }

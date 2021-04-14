40027d22b6eb27b0870ff13767f65d4be0bcf033
FethyAmr <74984977+FethyAmr@users.noreply.github.com>
2021-04-14 17:19:56 +0200

    bakery

--- chapitre-04/bakery/src/App.js
+++ chapitre-04/bakery/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Add from "./component/Add"
+import List from "./component/List"
+import Pay from "./component/Pay"
+import Button from "./component/Button"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      // activeTab: add,
+      // items: []
+    }
+  }
+
+  render() {
+    return (
+
+      <div className="container-fluid">
+        <div className="row">
+
+          <Add />
+          <List />
+          <Pay />
+          <Button />
+
+        </div>
+      </div>
+
+
+    );
+  }
 }
 
-export default App;
+export default App
---
+++ chapitre-04/bakery/src/component/Add.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+class Add extends React.Component {
+    render () {
+        return (
+            <div>
+                <h1>Add.jsx</h1>
+            </div>
+        )
+    }
+}
+export default Add

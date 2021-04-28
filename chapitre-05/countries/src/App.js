1b5825aa577acb7ce2c4ed951d0abd93fb0de3ef
FethyAmr <74984977+FethyAmr@users.noreply.github.com>
2021-04-28 17:33:26 +0200

    Countries

---
+++ chapitre-05/countries/src/App.js
@@ -0,0 +1,74 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Button from './componets/Button'
+
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      name: "",
+      capital: "",
+      flag: "",
+      population: "",
+      region: ""
+    }
+  }
+
+  componentDidMount() {
+    fetch("https://restcountries.eu/rest/v2/name/france")
+      .then(res => res.json())
+      .then(result => {
+        console.log(result)
+
+        this.setState({
+          name: result[0].name,
+          capital: result[0].capital,
+          flag: result[0].flag,
+          population: result[0].population,
+          region: result[0].region
+        })
+      })
+      .catch(error => console.error(error));
+  }
+
+  getCountry(country) {
+    fetch("https://restcountries.eu/rest/v2/name/", "country")
+      .then(res => res.json())
+      .then(result => {
+        console.log(result)
+
+        this.setState({
+          name: result[0].name,
+          capital: result[0].capital,
+          flag: result[0].flag,
+          population: result[0].population,
+          region: result[0].region
+        })
+      })
+      .catch(error => console.error(error));
+  }
+
+
+  render() {
+    return (
+      <div>
+
+        <h1>Country selector</h1>
+
+        <Button onClick={this.getCountry}>France</Button>
+        <Button onClick={this.getCountry} >Brazil</Button>
+        <Button onClick={this.getCountry} >Croatia</Button>
+
+        <p> Name : {this.state.name}</p>
+        <p> Capital : {this.state.capital}</p>
+        <p> Flag : {this.state.flag}</p>
+        <p> Population : {this.state.population}</p>
+        <p> Region : {this.state.region}</p>
+      </div >
+    )
+  }
+}
+
+export default App;
---
+++ chapitre-05/countries/src/componets/Button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+class Button extends React.Component {
+    render() {
+        return (
+                <button type="button" className="btn btn-primary" onClick = {this.props.onClick} 
+                        children={this.props.children}>
+                        </button>
+        )
+    }
+}
+export default Button;

58403d12e24d95c85e438a4477934f10cdd787cd
FethyAmr <74984977+FethyAmr@users.noreply.github.com>
2021-04-30 13:46:10 +0200

    Cards.jsx

--- chapitre-05/countries/src/componets/Card.jsx
+++ chapitre-05/countries/src/componets/Card.jsx
@@ -1,15 +1,40 @@
 import React from 'react'
+
 class Card extends React.Component {
+
     render() {
         return (
-            <div>
-                <img src={this.props.flag} alt="countries flag" style={{width: "200px"}}/>
-                <p>Name: {this.props.name}</p>
+            <div class="fw-bold" style={{ textAlign: 'center' }}>
+                <img src={this.props.flag}
+                    alt="countries flag"
+                    style={{ width: "150px" }} />
+                <br></br>
+                <p>Country: <br /> {this.props.name}</p>
                 <p>Capital: {this.props.capital}</p>
-                <p>Region: {this.props.region}</p>
-                <p>Population: {this.props.population}</p>
+                <p>Region: <br /> {this.props.region}</p>
+                <p>Population: <br /> {this.props.population}</p>
+                <div class="border mx-2 px-6" >
+                    <div style={{ textAlign: 'center' }}>
+                        <img style={{
+                            width: 180,
+                            borderTopLeftRadius: 4,
+                            borderTopRightRadius: 4
+                        }}
+                            src={this.props.flag}
+                            alt="countries flag" />
+                       
+                        <br />
+                        <br />
+                        <p style={{ fontWeight: 600, fontSize: 19 }}>Country: <br /> {this.props.name}</p>
+                        <p style={{ fontWeight: 500, fontSize: 17 }}>Capital: {this.props.capital}</p>
+                        <p style={{ fontWeight: 450, fontSize: 17 }}>Region: <br /> {this.props.region}</p>
+                        <p class="bi bi-people-fill" style={{ fontWeight: 450, fontSize: 17 }}>Population: <br /> {this.props.population}</p>
+                    </div>
+                </div>
             </div>
         )
     }
 }
 export default Card;
+
+
